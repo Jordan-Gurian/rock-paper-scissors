f33b72a9df5b581c098a99f181638655aee2927b
Jordan Gurian <gurianjordan@gmail.com>
2024-02-27 12:33:17 -0800

    Add UI to game with score keeping
    Add 3 buttons for user choice and to initiate new round

--- javascript.js
+++ javascript.js
@@ -1,63 +1,99 @@
+let rock = document.querySelector('#rock');
+rock.addEventListener('click', function() {
+    playRound('Rock', getComputerChoice())
+});
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', function(){
+    playRound('Paper', getComputerChoice());
+});
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', function(){
+    playRound('Scissors', getComputerChoice());
+});
+
+
 function getComputerChoice() {
     let computerOptions = [`Rock`, `Paper`, `Scissors`];
     return computerOptions[Math.floor(Math.random() * 3)]
 }
 
 function playRound(playerSelection, computerSelection) {
+    let playerScore = document.querySelector('.player-score > span');
+    let computerScore = document.querySelector('.computer-score > span');
+    let winner = document.querySelector('.winner-announce');
+
+    let results = document.querySelector('.results');
+    console.log(playerSelection);
     let playerSelectionUpper = playerSelection.toUpperCase();
     let computerSelectionUpper = computerSelection.toUpperCase();
     
     if (playerSelectionUpper === computerSelectionUpper) {
-        return `Draw!`
+        results.textContent = 'Draw!'
+        return
     }
     if (playerSelectionUpper === `ROCK`) {
         if (computerSelectionUpper === `SCISSORS`) {
-            return `You win! Rocks beats Scissors`
+            results.textContent = `You win! Rocks beats Scissors`;
+            playerScore.textContent++;
         }
         else {
-            return `You lose! Paper beats Rock`
+            results.textContent = `You lose! Paper beats Rock`;
+            computerScore.textContent++;
         }
     }
     else if (playerSelectionUpper === `PAPER`) {
         if (computerSelectionUpper === `ROCK`) {
-            return `You win! Paper beats Rock`
+            results.textContent = `You win! Paper beats Rock`;
+            playerScore.textContent++;
         }
         else {
-            return `You lose! Scissors beats Paper`
+            results.textContent = `You lose! Scissors beats Paper`;
+            computerScore.textContent++;
         }
     }
     else {
         if (computerSelectionUpper === `PAPER`) {
-            return `You win! Scissors beats Paper`
+            results.textContent = `You win! Scissors beats Paper`;
+            playerScore.textContent++;
         }
         else {
-            return `You lose! Rock beats Scissors`
+            results.textContent = `You lose! Rock beats Scissors`;
+            computerScore.textContent++;
         }
     }
+    if (winner.textContent == '' && Number(playerScore.textContent) === 5) {
+        winner.textContent = 'Congrats! You won 5 games first!';
+    }
+    else if (winner.textContent == '' && Number(computerScore.textContent) === 5) {
+        winner.textContent = 'Sorry! The computer won 5 games first!';
+    }
+
     return
 }
 
-function playGame() {
-    let playerWins = 0;
-    let computerWins = 0;
-    let winner = `No winner, this 5 game series is a draw!`;
+// function playGame() {
+//     let playerWins = 0;
+//     let computerWins = 0;
+//     let winner = `No winner, this 5 game series is a draw!`;
 
-    for (let i = 0; i < 5; i++) {
-        let result = playRound(prompt(`Pick Rock, Paper, or Scissors!`), getComputerChoice());
-        if (result.includes(`win`)) {
-            playerWins++;
-        }
-        else if (result.includes(`lose`)) {
-            computerWins++;
-        }
-        console.log(result);
-    }
-    if (playerWins > computerWins) {
-        winner = `You won the 5 game series!`;
-    }
-    else if (computerWins) {
-        winner = `You lost the 5 game series!`;
-    }
-    console.log(winner);
-    return
-}
+//     for (let i = 0; i < 5; i++) {
+//         let result = playRound(prompt(`Pick Rock, Paper, or Scissors!`), getComputerChoice());
+//         if (result.includes(`win`)) {
+//             playerWins++;
+//         }
+//         else if (result.includes(`lose`)) {
+//             computerWins++;
+//         }
+//         console.log(result);
+//     }
+//     if (playerWins > computerWins) {
+//         winner = `You won the 5 game series!`;
+//     }
+//     else if (computerWins) {
+//         winner = `You lost the 5 game series!`;
+//     }
+//     console.log(winner);
+//     return
+// }
